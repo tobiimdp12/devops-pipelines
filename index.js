8402c901d27bbb8a02822b1542ac790cb9a4a17c
tobiimdp12 <tobiasselva@gmail.com>
2026-01-13 13:48:17 -0300

    feature add CI Pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("DevOps Pipelines Module Loaded");
+console.log(" im learning ci cdf using github-actions")
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("starting tests");
+setTimeout((()=>{console.log("test 1")}),1000);
+console.log("tests finished");  
